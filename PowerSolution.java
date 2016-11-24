f219f365fff8a3aa9a55be2a05fa316d7d73d221
bishwas kalika <bishwas.kalika.c1@gmail.com>
2016-11-24 12:29:10 +0545

    dssd

---
+++ PowerSolution.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class PowerSolution here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+ 
+public class PowerSolution implements ActionListener
+{
+ArrayList <Generator> gen;
+    
+    
+}
